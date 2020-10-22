21020c1da3810b3daeb3c62d259ca734f1a7caa7
Iván López <ivan.lopezgon@iesdoctorbalmis.com>
2020-10-22 18:24:47 +0200

    Agregar archivos de proyecto.

---
+++ 7_TamañoTexto/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace _7_TamañoTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tamañoFuente_Checked(object sender, RoutedEventArgs e)
+        {
+            cambiarTextBlock.FontSize = int.Parse((sender as RadioButton).Tag.ToString());
+        }
+    }
+}
